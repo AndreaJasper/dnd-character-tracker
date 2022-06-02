115dceb6986e295b03c3b393b1d89a73bdd86730
AndreaJasper <andrea.nicole.jasper@gmail.com>
2022-06-02 14:18:02 -0600

    updates user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
   def index
   end
 
+  def create
+    if user = User.authenticate(params[:email], param[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back, #{user.name}!"
+        redirect_to(session[:intended_url] || user)
+        session[:intended_url] = nil
+      else
+        flash.now[:alert] = "Invalid email/password combination!"
+        render :new
+    end
+  end
+
   def show
     response_with current_user
   end
