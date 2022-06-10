ad17b4da9b09478955b80f261e75cce5559edfb5
AndreaJasper <andrea.nicole.jasper@gmail.com>
2022-06-10 10:16:00 -0600

    adds after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   layout 'application'
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for resource
+    stored_location_for(resource) || characters_path
+  end
 end
+
