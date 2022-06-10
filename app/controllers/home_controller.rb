5dceeb610dbd036ed4a63319deba5924f48928e4
AndreaJasper <andrea.nicole.jasper@gmail.com>
2022-06-10 10:20:49 -0600

    removes unnecessary code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,3 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
-  # before_action :check_auth
-
-  # def check_auth
-  #   unless user_signed_in?
-  #     redirect_to controller: :characters
-  #   end
-  # end
 end
